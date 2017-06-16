25628f3d44844b5e34284798fe7c0b9a5fa21018
Valentino <Valentino@172.20.248.50>
2017-06-16 22:59:05 +0200

    Esame_20160701_A

---
+++ src/it/polito/tdp/formulaone/db/FormulaOneDAO.java
@@ -0,0 +1,219 @@
+package it.polito.tdp.formulaone.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.polito.tdp.formulaone.model.Circuit;
+import it.polito.tdp.formulaone.model.Constructor;
+import it.polito.tdp.formulaone.model.Driver;
+import it.polito.tdp.formulaone.model.Season;
+
+
+public class FormulaOneDAO {
+
+	public List<Integer> getAllYearsOfRace() {
+		
+		String sql = "SELECT year FROM races ORDER BY year" ;
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			
+			ResultSet rs = st.executeQuery() ;
+			
+			List<Integer> list = new ArrayList<>() ;
+			while(rs.next()) {
+				list.add(rs.getInt("year"));
+			}
+			
+			conn.close();
+			return list ;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("SQL Query Error");
+		}
+	}
+	
+	public List<Season> getAllSeasons() {
+		
+		String sql = "SELECT year, url FROM seasons ORDER BY year" ;
+		
+		try {
+			Connection conn = DBConnect.getConnection() ;
+
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			
+			ResultSet rs = st.executeQuery() ;
+			
+			List<Season> list = new ArrayList<>() ;
+			while(rs.next()) {
+				list.add(new Season(Year.of(rs.getInt("year")), rs.getString("url"))) ;
+			}
+			
+			conn.close();
+			return list ;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	public List<Circuit> getAllCircuits() {
+
+		String sql = "SELECT circuitId, name FROM circuits ORDER BY name";
+
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+
+			List<Circuit> list = new ArrayList<>();
+			while (rs.next()) {
+				list.add(new Circuit(rs.getInt("circuitId"), rs.getString("name")));
+			}
+
+			conn.close();
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("SQL Query Error");
+		}
+	}
+	
+	public List<Constructor> getAllConstructors() {
+
+		String sql = "SELECT constructorId, name FROM constructors ORDER BY name";
+
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+
+			List<Constructor> constructors = new ArrayList<>();
+			while (rs.next()) {
+				constructors.add(new Constructor(rs.getInt("constructorId"), rs.getString("name")));
+			}
+
+			conn.close();
+			return constructors;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("SQL Query Error");
+		}
+	}
+	
+	public List<Driver> getDriversForSeason(Season s) {
+		String sql = "SELECT DISTINCT drivers.* " + 
+				"FROM races, results, drivers " + 
+				"WHERE races.year=? " + 
+				"AND results.raceId=races.raceId " + 
+				"AND results.position is not null " + 
+				"AND results.driverId=drivers.driverId";
+		
+		Connection conn = DBConnect.getConnection();
+		
+		try {
+		
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			st.setInt(1, s.getYear().getValue());
+			
+			ResultSet res = st.executeQuery();
+
+			List<Driver> result = new ArrayList<>();
+			
+			while(res.next()) {
+				Driver d = new Driver(
+						res.getInt("driverId"),
+						res.getString("driverref"),
+						res.getInt("number"),
+						res.getString("code"),
+						res.getString("forename"),
+						res.getString("surname"),
+						res.getDate("dob").toLocalDate(),
+						res.getString("nationality"),
+						res.getString("url") );
+				result.add(d);
+			}
+
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Conta il numero di vittorie d1{@code d1} su {@code d2} nella
+	 * stagione {@code s}
+	 * @param d1
+	 * @param d2
+	 * @param s
+	 * @return
+	 */
+	public Integer contaVittorie(Driver d1, Driver d2, Season s) {
+		String sql = "SELECT count(races.raceId) as cnt " + 
+				"FROM results r1, results r2, races " + 
+				"WHERE r1.raceId=r2.raceId " + 
+				"AND races.raceId=r1.raceId " + 
+				"AND races.year=? " + 
+				"AND r1.position<r2.position " + 
+				"AND r1.driverId=? " + 
+				"AND r2.driverId=?" ;
+		
+		Connection conn = DBConnect.getConnection();
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql) ;
+
+			st.setInt(1, s.getYear().getValue());
+			st.setInt(2, d1.getDriverId());
+			st.setInt(3, d2.getDriverId());
+			
+			ResultSet res = st.executeQuery() ;
+			
+			res.next() ;
+			
+			Integer result = res.getInt("cnt") ;
+			
+			conn.close();
+			return result ;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		FormulaOneDAO dao = new FormulaOneDAO() ;
+		
+		List<Integer> years = dao.getAllYearsOfRace() ;
+		System.out.println(years);
+		
+		List<Season> seasons = dao.getAllSeasons() ;
+		System.out.println(seasons);
+
+		
+		List<Circuit> circuits = dao.getAllCircuits();
+		System.out.println(circuits);
+
+		List<Constructor> constructors = dao.getAllConstructors();
+		System.out.println(constructors);
+		
+	}
+	
+}
---
+++ src/it/polito/tdp/formulaone/model/Model.java
@@ -0,0 +1,166 @@
+package it.polito.tdp.formulaone.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.formulaone.db.FormulaOneDAO;
+
+public class Model {
+
+	private List<Season> seasons ;
+	
+	private SimpleDirectedWeightedGraph<Driver, DefaultWeightedEdge> graph ;
+	
+	
+	// variabili di stato della ricorsione
+	int tassoMin ;
+	List<Driver> teamMin ;
+	
+	public List<Season> getSeasons() {
+		if(this.seasons == null) {
+			FormulaOneDAO dao = new FormulaOneDAO() ;
+			this.seasons = dao.getAllSeasons() ;
+		}
+		return this.seasons;
+	}
+	
+	public void creaGrafo(Season s){
+		
+		//create graph
+		this.graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class) ;
+		
+		FormulaOneDAO dao = new FormulaOneDAO() ;
+		
+		// add vertices
+		
+		List<Driver> drivers = dao.getDriversForSeason(s) ;
+		Graphs.addAllVertices(this.graph, drivers) ;
+		//System.out.println(this.graph);
+		
+		// add edges
+		for(Driver d1 : this.graph.vertexSet()) {
+			for(Driver d2 : this.graph.vertexSet()) {
+				if(!d1.equals(d2)) {
+					Integer vittorie = dao.contaVittorie(d1, d2, s) ;
+					if(vittorie>0) {
+						Graphs.addEdge(this.graph, d1, d2, vittorie) ;
+					}
+				}
+			}
+		}
+		
+		//System.out.println(this.graph);
+	}
+	
+	public void creaGrafo2(Season s){
+		/**
+		 * Versione alternativa
+		 */
+		String sql = "SELECT count(races.raceId), r1.driverId as d1, r2.driverId as d2\r\n" + 
+				"FROM results r1, results r2, races\r\n" + 
+				"WHERE r1.raceId=r2.raceId\r\n" + 
+				"AND races.raceId=r1.raceId\r\n" + 
+				"AND races.year=2000\r\n" + 
+				"AND r1.position<r2.position\r\n" + 
+				"GROUP BY d1, d2" ; 	
+	}
+	
+	public Driver getBestDriver() {
+		Driver best=null;
+		int max = Integer.MIN_VALUE;
+		
+		for(Driver d: this.graph.vertexSet()) {
+			int peso = 0;
+			
+			for(DefaultWeightedEdge e: graph.outgoingEdgesOf(d)) {
+				peso += graph.getEdgeWeight(e);
+			}
+			
+			for(DefaultWeightedEdge e: graph.incomingEdgesOf(d)) {
+				peso -= graph.getEdgeWeight(e);
+			}
+			
+			if(peso>max) {
+				max = peso;
+				best = d;
+			}	
+		}
+		
+		return best;
+	}
+	
+	public List<Driver> getDreamTeam(int K) {
+		
+		Set<Driver> team = new HashSet<>() ;
+		this.tassoMin = Integer.MAX_VALUE ;
+		this.teamMin = null;
+		
+		ricorsiva(0, team, K);
+		
+		return this.teamMin ;
+		
+	}
+	
+	/**
+	 * In ingresso ricevo il {@code team} parziale composto da {@code passo} elementi
+	 * La variabile {@code passo} parte da 0.
+	 * Il caso terminale è quando {@code passo==K}, ed in quel caso va calcolato il
+	 * tasso di sconfitta.
+	 * Altrimenti, si procede ricorsivamente ad aggiungere
+	 * un nuovo vertice(il passo+1-esimo), scegliendolo tra i vertici
+	 * non ancora presenti nel {@code team}.
+	 * 
+	 * @param passo
+	 * @param team
+	 * @param K
+	 */
+	private void ricorsiva(int passo, Set<Driver> team, int K) {
+		
+		// caso terminale?
+		if(passo==K) {
+			
+			// calcolare il tasso di sconfitta del team
+			int tasso = this.tassoSconfitta(team) ;
+			
+			// eventualmente aggiornare il minimo
+			if(tasso < tassoMin) {
+				tassoMin = tasso ;
+				teamMin = new ArrayList<>(team);
+			
+				//System.out.println(tassoMin + " "+team.toString()) ;
+			}
+		} else {
+			
+			// caso normale
+			Set<Driver> candidati = new HashSet<>(graph.vertexSet()) ;
+			candidati.removeAll(team) ;
+			
+			for(Driver d: candidati) {
+				team.add(d) ;
+				ricorsiva(passo+1, team, K) ;
+				team.remove(d);
+			}
+		
+		}
+	}
+
+	private int tassoSconfitta(Set<Driver> team) {
+		int tasso = 0 ;
+		
+		for(DefaultWeightedEdge e : this.graph.edgeSet()) {
+			if ( !team.contains(graph.getEdgeSource(e)) &&
+					team.contains(graph.getEdgeTarget(e)) ) {
+				tasso += graph.getEdgeWeight(e) ;
+			}
+		}
+		
+		return tasso ;
+	}
+	
+}
